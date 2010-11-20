78d89f202e3da758dec756c9feb5a039807b18a2
Dwight Bell <dbelll@yahoo.com>
2010-11-20 08:26:24 -0500

    initial project template

---
+++ mcar.h
@@ -0,0 +1,11 @@
+//
+//  mcar.h
+//  mcar
+//
+//  Created by Dwight Bell on 11/20/10.
+//  Copyright dbelll 2010. All rights reserved.
+//
+
+
+void gpu_operation(int n, float *x);
+void cpu_operation(int n, float *x);
