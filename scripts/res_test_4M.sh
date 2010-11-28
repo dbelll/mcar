04aca9966192cdf092a57b6ea8f8029a550187e7
Dwight Bell <dbelll@yahoo.com>
2010-11-28 06:19:28 -0500

    increase blocksize and no register limit on compile

---
+++ scripts/res_test_4M.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Testing script for resonance computer
+_trials="--TRIALS=16"
+_time="--TIME_STEPS=524288"
+_groups="--AGENT_GROUP_SIZE=256"
+_interval="--SHARING_INTERVAL=262144"
+_restart="--RESTART_INTERVAL=256"
+
+_run="--RUN_ON_CPU=0 --RUN_ON_GPU=1"
+_location="./bin/linux/release"
+
+_test="--TEST_INTERVAL=16384 --TEST_REPS=256 --TEST_MAX=300"
+_theta="--INIT_THETA_MIN=-0.10 --INIT_THETA_MAX=0.10"
+
+_params="--ALPHA=0.20 --EPSILON=0.10 --GAMMA=0.95 --LAMBDA=0.2"
+_h="--HIDDEN_NODES=1"
+
+_common="$_trials $_time $_groups $_interval $_restart $_run $_test $_theta"
+
+$_location/mcar $_common $_params $_h
+
+
